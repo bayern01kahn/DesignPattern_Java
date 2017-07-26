01b504ef2aca26c51ee3fc412bf48eddfa44c1ec
justin <155234557@qq.com>
2017-07-26 17:11:13 +0800

    组合模式

---
+++ DesignPattern/src/composite/Component.java
@@ -0,0 +1,15 @@
+package composite;
+
+public abstract class Component {
+
+	public String name;
+
+	public Component(String name) {
+		this.name = name;
+	}
+
+	// 公有操作
+	public void getName() {
+		System.out.println(this.name);
+	}
+}
---
+++ DesignPattern/src/composite/Leaf.java
@@ -0,0 +1,8 @@
+package composite;
+
+public class Leaf extends Component {
+
+	public Leaf(String name) {
+		super(name);
+	}
+}
