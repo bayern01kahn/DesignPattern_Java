c1512841ad3ffefd74ce5a57557a5c17d6866913
justin.luo <bayern01kahn@gmail.com>
2019-05-31 20:15:28 +0800

    更新多种单例模式实现与测试类

---
+++ DesignPattern/src/singleton/containerSingleton/Pojo.java
@@ -0,0 +1,4 @@
+package singleton.containerSingleton;
+
+public class Pojo {
+}
--- DesignPattern/src/singleton/enumSingleton_recommend/EnumSingleton.java
+++ DesignPattern/src/singleton/enumSingleton_recommend/EnumSingleton.java
@@ -1,34 +1,28 @@
 package singleton.enumSingleton_recommend;
 
+
 /**
  * ------------------------------------------------------------20190531--------------------------------------------------------------------
  * 最佳单例, (Effective java 作者推荐)
  * 不会存在 被反序列化破坏单例, jdk源码底层 针对针对枚举不被序列化和反射所破坏来保驾护航. (枚举类型不能被反射的方式来创建枚举类型的对象)
- *    反射时 会直接抛出枚举类型不能反射创建的异常.
+ *    反射时 会直接抛出枚举类型不能反射创建的异常.java.lang.IllegalArgumentException: Cannot reflectively create enum objects
+ *
+ *    测试类见, 同目录下的 EnumSingletonTest
+ *
  * 不会存在 线程安全问题
  * 不会存在 指令重排-初始化未完成就被其他使用而导致空指针异常
  * ----------------------------------------------------------------------------------------------------------------------------------------
  */
-public class EnumSingleton {
-
-    public EnumSingleton() {
+public enum EnumSingleton {
+    INSTANCE;
+    private Object data;
+    public Object getData() {
+        return data;
     }
-
-    private enum Singleton{
-        INSTANCE;
-
-        private final EnumSingleton instance;
-
-        Singleton(){
-            instance = new EnumSingleton();
-        }
-
-        public EnumSingleton getInstance(){
-            return instance;
-        }
+    public void setData(Object data) {
+        this.data = data;
     }
-
     public static EnumSingleton getInstance(){
-        return Singleton.INSTANCE.getInstance();
+        return INSTANCE;
     }
 }
